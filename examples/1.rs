afa6efb7945c12d7f574f8ead9fd1c23139bdc02
rap2hpoutre <raphaelht@gmail.com>
2016-08-21 23:32:02 +0200

    haha.

--- examples/1.rs
+++ examples/1.rs
@@ -1,13 +1,13 @@
 extern crate env_logger;
 extern crate futures;
-extern crate futures_minihttp;
+extern crate corruption;
 extern crate time;
 
 use std::net::SocketAddr;
 use std::env;
 
 use futures::*;
-use futures_minihttp::{Server, Response, Request};
+use corruption::{Server, Response, Request};
 
 fn main() {
     env_logger::init().unwrap();
